c5887e4665472f54c42d28fb40dae93a0960e8c0
Katherine Wong <t-kawong@microsoft.com>
2020-06-27 14:14:30 -0700

    mail to done ish

--- email-activism/src/Form.js
+++ email-activism/src/Form.js
@@ -11,35 +11,67 @@ class Form extends React.Component {
 
       this.state = {
         showdata : this.displayData,
-        postVal : ""
+        recipient : "",
+        subject: "",
+        emailBody: "",
+        description: ""
+
       }
 
-      this.prependData = this.prependData.bind(this);
-      this.handleChange = this.handleChange.bind(this);
+      this.post = this.post.bind(this);
+      this.handleChangeRecipient = this.handleChangeRecipient.bind(this);
+      this.handleChangeSubject = this.handleChangeSubject.bind(this);
+      this.handleChangeEmailBody = this.handleChangeEmailBody.bind(this);
+      this.handleChangeDescription = this.handleChangeDescription.bind(this);
+
+
+
 
     };
 
-  prependData() {
-   this.displayData.unshift(<div id="display-data"><pre>{this.state.postVal}</pre><a href="mailto:email@example.com?subject=testsubject">Send Email</a></div>);
+  post() {
+    var link = "mailto:" + this.state.recipient + "?subject=" + this.state.subject + "&body=" + this.state.emailBody;
+   this.displayData.unshift(<div id="display-data"><pre>To:{this.state.recipient}</pre><pre>Description:{this.state.description}</pre><a href={link}>Send Email</a></div>);
    this.setState({
       showdata : this.displayData,
-      postVal : ""
+      description : ""
    });
  }
 
- handleChange(e) {
+ handleChangeRecipient(e) {
       let getTextAreaValue = e.target.value;
       this.setState({
-        postVal :getTextAreaValue
+        recipient :getTextAreaValue
       });
+}
+handleChangeSubject(e) {
+  let getTextAreaValue = e.target.value;
+  this.setState({
+    subject :getTextAreaValue
+  });
+}
+handleChangeEmailBody(e) {
+  let getTextAreaValue = e.target.value;
+  this.setState({
+    emailBody :getTextAreaValue
+  });
+}
+handleChangeDescription(e) {
+  let getTextAreaValue = e.target.value;
+  this.setState({
+    description :getTextAreaValue
+  });
 }
 
   render() {
     return (
           <div id="mainContainer">
-             <textarea rows="4" cols="50" value={this.state.postVal} onChange={this.handleChange} ></textarea>
+             <textarea rows="1" cols="50" value={this.state.recipient} onChange={this.handleChangeRecipient} ></textarea>
+             <textarea rows="1" cols="50" value={this.state.subject} onChange={this.handleChangeSubject} ></textarea>
+             <textarea rows="3" cols="50" value={this.state.description} onChange={this.handleChangeDescription} ></textarea>
+             <textarea rows="10" cols="50" value={this.state.emailBody} onChange={this.handleChangeEmailBody} ></textarea>
              <div >
-             <input  type="submit" className="button" onClick={this.prependData}   value="Prepend"/>
+             <input  type="submit" className="button" onClick={this.post}   value="Prepend"/>
              </div>
              <div id="display-data-Container">
              {this.displayData}
