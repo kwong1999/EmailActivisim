8ec3d1cde760025f6db8bd41cd58c74d6c0a9356
Michelle Ran <ranmiche@usc.edu>
2020-06-27 20:07:06 -0400

    Add GET /template

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const database = require('./database');
 const express = require('express');
 const app = express();
 
+// Create new template
 app.post('/template', (req, res, next) => {
     database.addTemplate(req.body, err => {
         res.status(404).send(err);
@@ -10,12 +11,18 @@ app.post('/template', (req, res, next) => {
     });
 });
 
+// Get template by id
 app.get('/template', (req, res, next) => {
     database.getTemplate(req.query.id, err => {
         res.status(404).send(err);
     }, template => {
         res.send(template);
     });
+});
+
+// Get max n templates
+app.get('/templates', (req, res, next) => {
+    
 });
 
 module.exports = app;
--- server/database.js
+++ server/database.js
@@ -15,34 +15,22 @@ const config = {
     server: 'caihack.database.windows.net',
     options: {
         database: 'emailtemplates',
-        encrypt: true
+        encrypt: true,
+        rowCollectionOnDone: true
     }
 };
 
+// TODO: how to wait until connection is complete?
+
 const connection = new Connection(config);
+let didConnect = false;
 connection.on('connect', err => {
     if (err) {
         console.error(err);
     } else {
-        // TODO: ...
+        didConnect = true;
     }
 });
-
-const executeQuery = (query, onError, onCompletion) => {
-    let request = new Request(query, err => {
-        if (err) {
-            onError(err);
-        }
-    });
-
-    request.on("row", columns => {
-        columns.forEach(column => {
-            console.log("%s\t%s", column.metadata.colName, column.value);
-        });
-    });
-
-    connection.execSql(request);
-}
 
 const insert = 'INSERT INTO TemplateSchema.Templates (Recipient, SubjectLine, Body) VALUES (@recipient, @subjectLine, @body);'
 exports.addTemplate = (template, onError, onCompletion) => {
@@ -52,12 +40,33 @@ exports.addTemplate = (template, onError, onCompletion) => {
     request.addParameter('recipient', TYPES.VarChar, template.recipient);
     request.addParameter('subjectLine', TYPES.VarChar, template.subjectLine);
     request.addParameter('body', TYPES.VarChar, template.body);
+
+    while (!didConnect) {}
+
     connection.execSql(request);
     // TODO: capture & return template id?
 }
 
+const selectById = 'select * from TemplateSchema.Templates where TemplateId = @id;'
 exports.getTemplate = (id, onError, onCompletion) => {
-    
+    let request = new Request(selectById, err => {
+        if (err) {
+            onError(err);
+        }
+    });
+    request.addParameter('id', TYPES.Int, id);
+
+    request.on('row', columns => {
+        let template = {};
+        for (const column of columns) {
+            template[column.metadata.colName] = column.value;
+        }
+        onCompletion(template);
+    });
+
+    while (!didConnect) {}
+
+    connection.execSql(request);
 }
 
 exports.getTemplates = (max, onError, onCompletion) => {
